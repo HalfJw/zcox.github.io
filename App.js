a8c58120816ad5f00a3129851be30017f91c9c18
HalfJw <zcoc@ycp.edu>
2024-10-07 17:59:40 -0400

    lets see if this works

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css'; // Ensure Tailwind CSS is imported
+import Flower from './Flower';
+
+function App() {
+  return (
+    <div className="App">
+      <Flower />
+    </div>
+  );
+}
+
+export default App;
