497a7e61c9d6289fd941c488631ef9911a431533
HalfJw <zcoc@ycp.edu>
2024-10-08 18:40:52 -0400

    something

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Import your main App component
+import App from './App.js'; // Import your main App component
 import './index.css'; // Optional: Include your CSS file for global styles
 
 // Render the App component into the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BouquetPage />
   </React.StrictMode>,
   document.getElementById('root') // This ID should match the one in index.html
 );
